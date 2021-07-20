c0164c7687a9f54a7053b71c6bd517a09c08b0ee
magaAsef <magali.asef@hotmail.com>
2021-07-19 21:49:55 -0300

    aplico concepto de pool de conexiones

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -1,5 +1,8 @@
 package datos;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class Conexion {
@@ -7,8 +10,16 @@ public class Conexion {
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "admin";
 
+    public static DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASSWORD);
+        ds.setInitialSize(5);
+        return ds;
+    }
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        return getDataSource().getConnection();
     }
     public static void close(ResultSet rs) throws SQLException {
         rs.close();
